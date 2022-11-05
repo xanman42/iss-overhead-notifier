6c8c6387dfa55d7e59153de4c1d3f42e88f8d157
Xander Kirsten <xanderkirsten1@gmail.com>
2022-11-05 11:48:28 +0200

    better nesting inside functions

--- main.py
+++ main.py
@@ -10,11 +10,32 @@ password = 'wrhqffdfbtvdmuim'
 
 
 def is_close():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+    # Your position is within +5 or -5 degrees of the ISS position.
     if iss_latitude - 5 < MY_LAT < iss_latitude + 5 and iss_longitude - 5 < MY_LONG < iss_longitude + 5:
         return True
 
 
 def is_dark():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now()
+    current_hour = int(str(time_now).split(' ')[1].split(':')[0])
     if current_hour > sunset or current_hour < sunrise:
         return True
 
@@ -31,33 +52,7 @@ def send_mail():
             )
 
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-data = response.json()
-
-iss_latitude = float(data["iss_position"]["latitude"])
-iss_longitude = float(data["iss_position"]["longitude"])
-
-# Your position is within +5 or -5 degrees of the ISS position.
-
-
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0,
-}
-
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
-
-time_now = datetime.now()
-current_hour = int(str(time_now).split(' ')[1].split(':')[0])
-
 send_mail()
 # If the ISS is close to my current position
 # and it is currently dark
 # Then send me an email to tell me to look up.
-# BONUS: run the code every 60 seconds.
